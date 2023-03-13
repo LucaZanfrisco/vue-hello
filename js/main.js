3b9a2c5f24c2da8c1f56af5d177c9c38d0504d5c
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-03-13 17:02:26 +0100

    aggiunto bottone che mostra l'immagine

--- js/main.js
+++ js/main.js
@@ -9,7 +9,13 @@ createApp({
       titolo2: '<span style= "color: red">C era una volta il sole</span>',
       titoloId: 'container',
       url: 'https://www.civitatis.com/f/italia/gaeta/gaeta.jpg',
-      imgId: 'imgScale'
-    };
+      imgId: 'imgScale',
+      imgD: 'imgHidden'
+    }  
   },
+  methods: {
+      mostra(){
+        this.imgD = 'imgShow'
+      }
+    }
 }).mount("#app");
